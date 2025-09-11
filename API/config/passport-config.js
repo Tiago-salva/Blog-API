3c3418918d94b7411c5dd758a87ce73b6f2dc504
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-09-11 20:14:46 -0300

    feat: add passport configuration for local authentication

---
+++ API/config/passport-config.js
@@ -0,0 +1,42 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+function initializePassport(passport) {
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "email" },
+      async (email, password, done) => {
+        try {
+          const user = await prisma.user.findUnique({ where: { email } });
+          if (!user) {
+            return done(null, false, { message: "User not found" });
+          }
+
+          const match = bcrypt.compare(password, user.password);
+          if (!match) {
+            return done(null, false, { message: "Incorrect password" });
+          }
+
+          return done(null, user);
+        } catch (err) {
+          return done(err);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await prisma.user.findUnique({ where: { id } });
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+}
+
+module.exports = initializePassport;
