f47efeefa72746f3362c81d9e93971acff046ca0
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-09-12 20:35:54 -0300

    feat: implement JWT authentication strategy

--- API/config/passport-config.js
+++ API/config/passport-config.js
@@ -1,7 +1,10 @@
 const LocalStrategy = require("passport-local").Strategy;
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
 const bcrypt = require("bcryptjs");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+require("dotenv").config();
 
 function initializePassport(passport) {
   passport.use(
@@ -14,7 +17,7 @@ function initializePassport(passport) {
             return done(null, false, { message: "User not found" });
           }
 
-          const match = bcrypt.compare(password, user.password);
+          const match = await bcrypt.compare(password, user.password);
           if (!match) {
             return done(null, false, { message: "Incorrect password" });
           }
@@ -27,16 +30,38 @@ function initializePassport(passport) {
     )
   );
 
-  passport.serializeUser((user, done) => done(null, user.id));
+  // JWT
+  const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  };
+  passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      try {
+        const user = await prisma.user.findUnique({
+          where: { id: jwt_payload.id },
+        });
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      } catch (err) {
+        return done(err, false);
+      }
+    })
+  );
 
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const user = await prisma.user.findUnique({ where: { id } });
-      done(null, user);
-    } catch (err) {
-      done(err);
-    }
-  });
+  // passport.serializeUser((user, done) => done(null, user.id));
+
+  // passport.deserializeUser(async (id, done) => {
+  //   try {
+  //     const user = await prisma.user.findUnique({ where: { id } });
+  //     done(null, user);
+  //   } catch (err) {
+  //     done(err);
+  //   }
+  // });
 }
 
 module.exports = initializePassport;
