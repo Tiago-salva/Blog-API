27735305053b7c0a725366571e3c653d3afcfcf0
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-09-12 20:36:11 -0300

    feat: add logout route and integrate JWT token generation on login

--- API/src/routes/authRouter.js
+++ API/src/routes/authRouter.js
@@ -1,25 +1,36 @@
 const { Router } = require("express");
-const { createUserPost } = require("../controllers/authController");
+const { createUserPost, logOut } = require("../controllers/authController");
 const passport = require("passport");
 const authRouter = new Router();
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
 
 // Login
 authRouter.post("/login", (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) return next(err);
-
-    if (!user) {
+    if (!user)
       return res.status(401).json({ message: "Invalid email or password" });
-    }
 
-    req.logIn(user, (err) => {
-      if (err) return next(err);
-      return res.json({ message: "Login successful", user });
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token: token,
+      user: { id: user.id, email: user.email, role: user.role },
     });
   })(req, res, next);
 });
 
 // Sign up
 authRouter.post("/sign-up", createUserPost);
+
+// Log out
+authRouter.post("/logout", logOut);
 
 module.exports = authRouter;
