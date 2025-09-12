378696be903f14779b16ebd9f705613154c7639d
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-09-12 20:35:30 -0300

    refactor: remove express-session and related session store configuration

--- API/app.js
+++ API/app.js
@@ -3,9 +3,7 @@ const path = require("node:path");
 require("dotenv").config();
 
 const express = require("express");
-const expressSession = require("express-session");
 const passport = require("passport");
-const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const { PrismaClient } = require("@prisma/client");
 
 // Custom config
@@ -13,6 +11,7 @@ const initializePassport = require("./config/passport-config");
 
 // Import routers
 const authRouter = require("./src/routes/authRouter");
+const postRouter = require("./src/routes/postRouter");
 
 // App
 const app = express();
@@ -28,25 +27,8 @@ app.use(express.static(__dirname));
 // Passport
 initializePassport(passport);
 
-app.use(
-  expressSession({
-    cookie: {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    },
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    store: new PrismaSessionStore(new PrismaClient(), {
-      checkPeriod: 2 * 60 * 1000,
-      dbRecordIdIsSessionId: true,
-      dbRecordIdFunction: undefined,
-    }),
-  })
-);
-
-app.use(passport.session());
-
 // Routes
 app.use("/", authRouter);
+app.use("/posts", postRouter);
 
 app.listen(3000, () => console.log("App listening to port 3000"));
