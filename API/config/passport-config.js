86e5c1403aecaeef470c2d8b6f50ab907068f6ae
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-09-13 21:28:53 -0300

    refactor: remove unused serialization and deserialization methods

--- API/config/passport-config.js
+++ API/config/passport-config.js
@@ -51,17 +51,6 @@ function initializePassport(passport) {
       }
     })
   );
-
-  // passport.serializeUser((user, done) => done(null, user.id));
-
-  // passport.deserializeUser(async (id, done) => {
-  //   try {
-  //     const user = await prisma.user.findUnique({ where: { id } });
-  //     done(null, user);
-  //   } catch (err) {
-  //     done(err);
-  //   }
-  // });
 }
 
 module.exports = initializePassport;
